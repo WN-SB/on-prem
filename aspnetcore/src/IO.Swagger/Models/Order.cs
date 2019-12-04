ff93cc55e9d6571dd33700b72b7a05876a87b1f9
Wesley.Nutley <wesley.nutley@smartbear.com>
2019-12-04 17:56:55 +0000

    Generated code by SwaggerHub

--- aspnetcore/src/IO.Swagger/Models/Order.cs
+++ aspnetcore/src/IO.Swagger/Models/Order.cs
@@ -1,7 +1,7 @@
 /*
  * Swagger Petstore
  *
- * This is a sample Petstore server.  You can find out more about Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/). 
+ *  Thiz is a sample Petstore server.  You can find out more about Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/). 
  *
  * OpenAPI spec version: 1.0.0
  * Contact: apiteam@swagger.io
